1822b0d129411b4b8a6699d312a7c58deece98a4
Ki-Hwan Oh <sktlgt93@gmail.com>
2025-04-19 09:54:31 -0500

    Section 02 complete.
    Section 02 complete.

--- section02_pytorch_basics/notebooks/load.py
+++ section02_pytorch_basics/notebooks/load.py
@@ -1,126 +1,87 @@
-
-import urllib.request
-import gzip
-import pickle
-import os
+import torch
+from torchvision import datasets, transforms
 import numpy as np
-
 from PIL import Image
-
-def _download(file_name):
-    file_path = dataset_dir + "/" + file_name
-
-    if os.path.exists(file_path):
-        return
-
-    print("Downloading " + file_name + " ... ")
-    urllib.request.urlretrieve(url_base + file_name, file_path)
-    print("Done")
-
-def download_mnist():
-    for v in key_file.values():
-        _download(v)
-
-def _load_label(file_name):
-    file_path = dataset_dir + "/" + file_name
-
-    print("Converting " + file_name + " to NumPy Array ...")
-    with gzip.open(file_path, 'rb') as f:
-            labels = np.frombuffer(f.read(), np.uint8, offset=8)
-    print("Done")
-
-    return labels
-
-def _load_img(file_name):
-    file_path = dataset_dir + "/" + file_name
-
-    print("Converting " + file_name + " to NumPy Array ...")
-    with gzip.open(file_path, 'rb') as f:
-            data = np.frombuffer(f.read(), np.uint8, offset=16)
-    data = data.reshape(-1, img_size)
-    print("Done")
-
-    return data
-
-def _convert_numpy():
-    dataset = {}
-    dataset['train_img'] =  _load_img(key_file['train_img'])
-    dataset['train_label'] = _load_label(key_file['train_label'])
-    dataset['test_img'] = _load_img(key_file['test_img'])
-    dataset['test_label'] = _load_label(key_file['test_label'])
-
-    return dataset
-
-def init_mnist():
-    download_mnist()
-    dataset = _convert_numpy()
-    print("Creating pickle file ...")
-    with open(save_file, 'wb') as f:
-        pickle.dump(dataset, f, -1)
-    print("Done")
-
-def _change_ont_hot_label(X):
-    T = np.zeros((X.size, 10))
-    for idx, row in enumerate(T):
-        row[X[idx]] = 1
-
-    return T
 
 def load_mnist(normalize=True, flatten=True, one_hot_label=False):
     """
+    Loads the MNIST dataset using PyTorch's torchvision.
+
     Parameters
     ----------
-    normalize : Normalize the pixel values
-    flatten : Flatten the images as one array
-    one_hot_label : Encode the labels as a one-hot array
+    normalize : bool
+        Whether to scale the image pixel values to [0, 1].
+    flatten : bool
+        Whether to flatten the 28x28 images to 784-dimensional vectors.
+    one_hot_label : bool
+        Whether to convert labels to one-hot encoding.
 
     Returns
     -------
-    (Trainig Image, Training Label), (Test Image, Test Label)
+    (train_x, train_y), (test_x, test_y) : Tuple of numpy arrays
     """
-    if not os.path.exists(save_file):
-        init_mnist()
+    transform_list = [transforms.ToTensor()]  # ToTensor scales pixels to [0, 1]
+    transform = transforms.Compose(transform_list)
 
-    with open(save_file, 'rb') as f:
-        dataset = pickle.load(f)
+    # Load datasets
+    train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
+    # Convert images and labels to NumPy arrays
+    train_x = train_dataset.data.numpy()
+    train_y = train_dataset.targets.numpy()
+    test_x = test_dataset.data.numpy()
+    test_y = test_dataset.targets.numpy()
+
+    # Normalize if requested
     if normalize:
-        for key in ('train_img', 'test_img'):
-            dataset[key] = dataset[key].astype(np.float32)
-            dataset[key] /= 255.0
+        train_x = train_x.astype("float32") / 255.0
+        test_x = test_x.astype("float32") / 255.0
 
-    if not flatten:
-         for key in ('train_img', 'test_img'):
-            dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
+    # Flatten if requested
+    if flatten:
+        train_x = train_x.reshape(-1, 28 * 28)
+        test_x = test_x.reshape(-1, 28 * 28)
+    else:
+        train_x = train_x.reshape(-1, 1, 28, 28)
+        test_x = test_x.reshape(-1, 1, 28, 28)
 
+    # One-hot encode labels if requested
     if one_hot_label:
-        dataset['train_label'] = _change_ont_hot_label(dataset['train_label'])
-        dataset['test_label'] = _change_ont_hot_label(dataset['test_label'])
+        train_y = np.eye(10)[train_y]
+        test_y = np.eye(10)[test_y]
 
-    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label'])
+    return (train_x, train_y), (test_x, test_y)
+
+def _change_one_hot_label(labels):
+    """
+    Converts label array to one-hot encoded format.
+
+    Parameters
+    ----------
+    labels : array of int
+
+    Returns
+    -------
+    one_hot_labels : array of shape (n_samples, 10)
+    """
+    return np.eye(10)[labels]
 
 def img_show(img):
-    pil_img = Image.fromarray(np.uint8(img))
+    """
+    Displays a single image (flattened or 2D array).
+
+    Parameters
+    ----------
+    img : numpy array of shape (784,) or (28, 28)
+    """
+    if img.ndim == 1:
+        img = img.reshape(28, 28)
+    pil_img = Image.fromarray(np.uint8(img * 255))  # Assume input is normalized
     pil_img.show()
-
-
-# Load the MNIST dataset
-url_base = 'https://ossci-datasets.s3.amazonaws.com/mnist/'
-key_file = {
-    'train_img':'train-images-idx3-ubyte.gz',
-    'train_label':'train-labels-idx1-ubyte.gz',
-    'test_img':'t10k-images-idx3-ubyte.gz',
-    'test_label':'t10k-labels-idx1-ubyte.gz'
-}
-
-dataset_dir = "../../datasets/MNIST/raw"
-save_file = dataset_dir + "/mnist.pkl"
 
 train_num = 60000
 test_num = 10000
 img_dim = (1, 28, 28)
 img_size = 784
-
-(train_x, train_y), (test_x, test_y) = load_mnist(normalize=False, flatten=True)
-
-
+(train_x, train_y), (test_x, test_y) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
